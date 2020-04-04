2b961eacacb4bd4e81248bc2a81d29c98ff8191a
Sakshi Srivastava <sakshisrivastava413@gmail.com>
2020-04-04 13:20:29 +0530

    changed solr install

--- scripts/init-solr.sh
+++ scripts/init-solr.sh
@@ -3,8 +3,4 @@
 if test -z $MINE; then
     MINE=intermine
 fi
-
-wget http://archive.apache.org/dist/lucene/solr/7.2.1/solr-7.2.1.tgz
-tar xzf solr-7.2.1.tgz && ./solr-7.2.1/bin/solr start -force
-./solr-7.2.1/bin/solr create -c $MINE-search -force
-./solr-7.2.1/bin/solr create -c $MINE-autocomplete -force
+curl https://raw.githubusercontent.com/apache/lucene-solr/releases/lucene-solr/7.3.0/solr/bin/solr -o solr
